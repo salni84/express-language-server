593bae3a7c5a298eab83f8c3c4cf0a0030a1f76e
TCHSARO1 <roberto.salerno@swisscom.com>
2023-10-07 21:00:41 +0200

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 let cors = require('cors');
 app.use(cors(
     {credentials: true}
